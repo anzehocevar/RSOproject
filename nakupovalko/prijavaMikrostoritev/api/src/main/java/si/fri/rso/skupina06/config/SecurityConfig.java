5f39916f52beb5b754179fef4a8dfeca8bef2216
Foovo <matek.veronika@gmail.com>
2024-10-27 19:52:52 +0100

    Add basic Prijava frontend

--- nakupovalko/prijavaMikrostoritev/api/src/main/java/si/fri/rso/skupina06/config/SecurityConfig.java
+++ nakupovalko/prijavaMikrostoritev/api/src/main/java/si/fri/rso/skupina06/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package si.fri.rso.skupina06.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -55,6 +58,11 @@ public class SecurityConfig {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
         return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).build();
     }
 
     /**
