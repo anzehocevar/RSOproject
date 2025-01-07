bc61c25f76446ac70a50f97d34a1e0cf81b8ca41
anzehocevar <anze.hocevar@gmail.com>
2025-01-07 18:23:28 +0100

    "docker running with friends(sql,prometheus,graphana)"

--- Item/src/main/java/skupina06/item/config/SecurityConfig.java
+++ Item/src/main/java/skupina06/item/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package skupina06.item.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,16 +12,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/actuator/**"
-                        ).permitAll() // Allow access to public endpoints
-                        .anyRequest().authenticated() // Authenticate other requests
+                        .anyRequest().permitAll() // Permit all requests
                 );
 
         return http.build();
     }
 }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Disable CSRF
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//
+//                                "/swagger-ui/**",
+//                                "/v3/api-docs/**",
+//                                "/actuator/**",
+//                                "/items",
+//                                "/items/**"
+//                        ).permitAll() // Allow access to public endpoints
+//                        .anyRequest().authenticated() // Authenticate other requests
+//                );
+//
+//        return http.build();
+//    }
+//}
